f91e7a02c980f62b265cf3beef21210398b0991a
Daniel Carvalho de Assis <dan.c.assis@gmail.com>
2024-03-06 00:03:32 -0300

    add calendar

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function updateClock() {
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
  
-
 h = h <10 ? "0"+ h : h;
 m = m <10 ? "0"+ m : m;
 s = s <10 ? "0"+ s : s;
@@ -32,16 +31,19 @@ const mes = document.getElementById("month")
 const ano = document.getElementById("year")
 
 function updateData() {
-    let today = new Date().getUTCDate()
-    let ms = new Date().getMonth() +1
-    let y = new Date().getFullYear()
-
-    today = today <10 ? "0"+ today : today;
+    const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    
+    let today = new Date().getDate()
+    let ms = meses[new Date().getMonth()]
+    let y = new Date().getFullYear()   
+    
+    today = today <10 ? "0" + today : today;
     ms = ms <10 ? "0"+ ms : ms;
-
+ 
     dia.innerText= today;
-    mes.innerText= ms;
+    mes.innerText = ms; 
     ano.innerText= y;
+
 }
 
 updateData()
