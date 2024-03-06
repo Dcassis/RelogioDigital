57bcf42ccf013241a2946284a38e5b2173d32e19
Daniel Carvalho de Assis <129464981+Dcassis@users.noreply.github.com>
2024-03-05 23:12:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// JS DO RELÓGIO
+
 const hours = document.getElementById("hours")
 const min = document.getElementById("minutes")
 const sec = document.getElementById("seconds")
@@ -23,4 +25,24 @@ s = s <10 ? "0"+ s : s;
 
 updateClock()
 
+//JS DO CALENDÁRIO
+
+const dia = document.getElementById("day")
+const mes = document.getElementById("month")
+const ano = document.getElementById("year")
+
+function updateData() {
+    let today = new Date().getUTCDate()
+    let ms = new Date().getMonth() +1
+    let y = new Date().getFullYear()
+
+    today = today <10 ? "0"+ today : today;
+    ms = ms <10 ? "0"+ ms : ms;
+
+    dia.innerText= today;
+    mes.innerText= ms;
+    ano.innerText= y;
+}
+
+updateData()
 
